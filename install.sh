3ed3fa42d5ee8005d08e29ffe387e5aeda999d49
Scott Hyndman <scotty.hyndman@gmail.com>
2024-05-13 18:12:20 -0400

    Update URLs to point to our fork

--- install.sh
+++ install.sh
@@ -63,7 +63,7 @@ function check_download {
 
     if [ ! -d "${K_SHAKETUNE_PATH}" ]; then
         echo "[DOWNLOAD] Downloading Klippain Shake&Tune module repository..."
-        if git -C $shaketunedirname clone https://github.com/Frix-x/klippain-shaketune.git $shaketunebasename; then
+        if git -C $shaketunedirname clone https://github.com/shyndman/klippain-shaketune.git $shaketunebasename; then
             chmod +x ${K_SHAKETUNE_PATH}/install.sh
             printf "[DOWNLOAD] Download complete!\n\n"
         else
@@ -105,7 +105,7 @@ function link_extension {
 function link_gcodeshellcommandpy {
     if [ ! -f "${KLIPPER_PATH}/klippy/extras/gcode_shell_command.py" ]; then
         echo "[INSTALL] Downloading gcode_shell_command.py Klipper extension needed for this module"
-        wget -P ${KLIPPER_PATH}/klippy/extras https://raw.githubusercontent.com/Frix-x/klippain/main/scripts/gcode_shell_command.py
+        wget -P ${KLIPPER_PATH}/klippy/extras https://raw.githubusercontent.com/shyndman/klippain/main/scripts/gcode_shell_command.py
     else
         printf "[INSTALL] gcode_shell_command.py Klipper extension is already installed. Continuing...\n\n"
     fi
